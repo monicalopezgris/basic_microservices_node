8a26a2c9bc713d67e270df227ab7c429d94963bb
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2021-02-02 23:51:50 +0100

    add event log

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,15 +5,30 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req,res) => {
   const event = req.body;
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  events.push(event);
+  axios.post("http://localhost:4000/events", event).catch((err)=>{
+    console.log(err);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err)=>{
+    console.log(err);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err)=>{
+    console.log(err);
+  });
+  axios.post("http://localhost:4003/events", event).catch((err)=>{
+    console.log(err);
+  });
   res.send({ status: "OK" });
 });
 
+app.get("/events", (req, res)=>{
+  res.send(events);
+})
+
 app.listen(4005, ()=>{
-    console.log('Listening 4005 - Event Bus')
+    console.log("Listening 4005 - Event Bus")
 })
